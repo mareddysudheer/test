9244f168517e38f4e8837e71ca9f85a662c839e4
sudheermareddy <sudheerm@sysgain.com>
2017-08-04 15:30:44 +0530

    Update workstaionsetup2.ps1

--- workstaionsetup2.ps1
+++ workstaionsetup2.ps1
@@ -1,3 +1,4 @@
+
 param(
 [string] $adminusername = "$1",
 [string] $adminPassword = "$2",
@@ -6,13 +7,15 @@ param(
 [string] $splunkfqdn = "$5"
 #[string] $wsNodeFqdn= "$6"
 )
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned  -Force
+cd C:\opscode\chefdk\bin
 chef generate app c:\Users\chef-repo
-echo c:\Users\chef-repo\.chef\knife.rb | knife configure --server-url https://$ChefServerFqdn/organizations/$organizationName/ --validation-client-name $organizationName-validator --validation-key c:/Users/chef-repo/.chef/$organizationName-validator.pem --user $chefServerUserName --repository c:/Users/chef-repo 
-knife ssl fetch --config c:\Users\chef-repo\.chef\knife.rb
-echo n | & "C:\Program Files\PuTTY\pscp.exe"  -scp -pw $adminPassword ${adminusername}@${ChefServerFqdn}:/etc/opscode/chefautomatedeliveryuser.pem C:\Users\chef-repo\.chef\$chefServerUserName".pem"
-echo n | & "C:\Program Files\PuTTY\pscp.exe"  -scp -pw $adminPassword ${adminusername}@${ChefServerFqdn}:/etc/opscode/chef-automate-org-validator.pem C:\Users\chef-repo\.chef\$organizationName".pem"
+echo c:\Users\chef-repo\.chef\knife.rb | knife configure --server-url https://$ChefAutoFqdn/organizations/$orguser --validation-client-name $orguser-validator --validation-key c:/Users/chef-repo/.chef/$orguser-validator.pem --user $adminUsername --repository c:/Users/chef-repo
+echo n | & "C:\Program Files\PuTTY\pscp.exe"  -scp -pw $adminPassword ${adminUsername}@${ChefAutoFqdn}:/etc/opscode/$adminUsername".pem" C:\Users\chef-repo\.chef\$adminUsername".pem"
+echo n | & "C:\Program Files\PuTTY\pscp.exe"  -scp -pw $adminPassword ${adminUsername}@${ChefAutoFqdn}:/etc/opscode/$orguser-validator.pem C:\Users\chef-repo\.chef\$orguser-validator.pem
+knife ssl  fetch --config c:\Users\chef-repo\.chef\knife.rb  --server-url https://$ChefAutoFqdn/organizations/$orguser
 git clone https://github.com/sysgain/chef-automate-hol-cookbooks.git C:/Users/cookbookstore
-cp -r C:/Users/cookbookstore/* C:\Users\chef-repo\cookbooks
+cp -r C:\Users\cookbookstore\* C:\Users\chef-repo\cookbooks
 (Get-Content C:\Users\chef-repo\cookbooks\splunk-uf-install/recipes/default.rb) ` | %{ $_ -replace 'localhost',$splunkfqdn} ` | Set-Content C:\Users\chef-repo\cookbooks\splunk-uf-install/recipes/default.rb
 knife bootstrap windows winrm localhost --config c:\Users\chef-repo\.chef\knife.rb -x $adminusername  -P $adminPassword -N splunkNode 
 knife cookbook upload --config c:\Users\chef-repo\.chef\knife.rb --server-url https://$ChefServerFqdn/organizations/$organizationName/ splunk-uf-install compat_resource audit 
