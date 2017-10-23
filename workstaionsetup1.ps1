60e3b345d3e8d03d06c1d5fdc12987dae35f8a5c
sudheermareddy <sudheerm@sysgain.com>
2017-10-23 10:51:55 +0530

    Update workstaionsetup1.ps1

--- workstaionsetup1.ps1
+++ workstaionsetup1.ps1
@@ -3,4 +3,4 @@ Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 setx PATH "$env:path;C:\opscode\chefDK\embedded\bin\" -m
 Invoke-WebRequest -Uri https://the.earth.li/~sgtatham/putty/latest/w64/putty-64bit-0.70-installer.msi -OutFile c:/users/Putty.msi 
 Start-Process c:/Users/Putty.msi   /qn -Wait
-Restart-Computer
+#Restart-Computer
