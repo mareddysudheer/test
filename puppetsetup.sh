8662cac99b960b9d7811bffdadb609ef6c43b06a
sudheermareddy <sudheerm@sysgain.com>
2017-12-07 23:16:12 +0530

    Update puppetsetup.sh

--- puppetsetup.sh
+++ puppetsetup.sh
@@ -24,4 +24,5 @@ sudo echo "puppet.master.com" > /etc/hostname
 sudo /home/ubuntu/puppet-enterprise-2017.3.2-ubuntu-16.04-amd64/puppet-enterprise-installer -c /home/ubuntu/puppet-enterprise-2017.3.2-ubuntu-16.04-amd64/conf.d/pe.conf
 sudo puppet agent -t
 echo " Now Puppet enterprise is ready"
+sudo puppet module install puppetlabs-ntp
 exit 0
