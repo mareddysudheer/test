e078f0ba20fb15fc61d9b8571e478aee3659ebc0
sudheermareddy <sudheerm@sysgain.com>
2017-12-27 21:03:24 +0530

    Update puppetsetup.sh

--- puppetsetup.sh
+++ puppetsetup.sh
@@ -26,14 +26,6 @@ sudo puppet agent -t
 sleep 20
 
 sudo puppet module install puppetlabs-ntp
-while :
-do
-  curl -k -s https://localhost:8140/status/v1/services | python -c 'import json,sys;obj=json.load(sys.stdin);sys.exit(0) if (obj["pe-master"]["state"] == "running") else sys.exit(1);'
-  if [[ $? == 0 ]]; then
-    exit
-  fi
-  sleep 5
-done
-sleep 3m
+sudo puppet agent -t
 echo " Puppet enterprise is ready"
 exit 0
